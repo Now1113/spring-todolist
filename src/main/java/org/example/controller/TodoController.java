931880e8119bc9cea501aacad6206469db5edb46
now <guswp1450@naver.com>
2022-03-19 01:19:05 +0900

    Commit

--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -2,12 +2,15 @@ package org.example.controller;
 
 import lombok.AllArgsConstructor;
 import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
 import org.example.model.TodoResponse;
 import org.example.service.TodoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 //
 @CrossOrigin
@@ -16,41 +19,59 @@ import java.util.List;
 @RequestMapping("/")
 public class TodoController {
 
-    private final TodoService todoService;
+    private final TodoService service;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> create() {
-        System.out.println("create");
-        return null;
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request) {
+        System.out.println("CREATE");
+
+        if (ObjectUtils.isEmpty(request.getTitle()))
+            return ResponseEntity.badRequest().build();
+
+        if (ObjectUtils.isEmpty(request.getOrder()))
+            request.setOrder(0L);
+
+        if (ObjectUtils.isEmpty(request.getCompleted()))
+            request.setCompleted(false);
+
+        TodoEntity result = this.service.add(request);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<List<TodoResponse>> readOne() {
-        System.out.println("read one");
-        return null;
+    public ResponseEntity<TodoResponse> readOne(@PathVariable Long id) {
+        System.out.println("READ ONE");
+        TodoEntity result = this.service.searchById(id);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping
-    public ResponseEntity<List<TodoEntity>> readAll() {
-        System.out.println("read all");
-        return null;
+    public ResponseEntity<List<TodoResponse>> readAll() {
+        System.out.println("READ ALL");
+        List<TodoEntity> list = this.service.searchAll();
+        List<TodoResponse> response = list.stream().map(TodoResponse::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<TodoEntity> update() {
-        System.out.println("update");
-        return null;
+    public ResponseEntity<TodoEntity> update(@PathVariable Long id, @RequestBody TodoRequest request) {
+        System.out.println("UPDATE");
+        TodoEntity result = this.service.updateById(id, request);
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteOne() {
-        System.out.println("delete one");
-        return null;
+    public ResponseEntity<?> deleteOne(@PathVariable Long id) {
+        System.out.println("DELETE ONE");
+        this.service.deleteById(id);
+        return  ResponseEntity.ok().build();
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteAll() {
-        System.out.println("delete all");
-        return null;
+        System.out.println("DELETE ALL");
+        this.service.deleteAll();
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/example/model/TodoEntity.java
+++ src/main/java/org/example/model/TodoEntity.java
@@ -26,13 +26,14 @@ public class TodoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false)
     private String title;
-    
-    //order 키워드는 H2 데이터베이스에서 이미 예약어로 사용되기에 name를 새로 지정
+
+    //order 키워드는 H2 데이터베이스에서 이미 예약어로 사용되기에 name 을 새로 지정
     @Column(name = "todoOrder", nullable = false)
     private Long order;
-    
+
+    @Column(nullable = false)
     private Boolean completed;
 }
